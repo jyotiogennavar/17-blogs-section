c57dacca8b87b1d73c71abe7ffa81d2cf875df9d
Jyoti <jyoti.ogennavar@xecurify.com>
2024-04-17 09:26:33 +0530

    Refactor project structure and dependencies

---
+++ src/BlogsSection/Index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styled from 'styled-components'
+import {motion} from 'framer-motion'
+
+const Index = () => {
+  return (
+    <div>Index</div>
+  )
+}
+
+export default Index
